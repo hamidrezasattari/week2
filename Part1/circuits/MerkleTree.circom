82d480c068049cbf9420076f197cb4929a99d0b3
hamid <hamidrezasattari2012@gmail.com>
2022-05-16 13:14:23 -0400

    week 2 commit

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -5,8 +5,25 @@ include "../node_modules/circomlib/circuits/poseidon.circom";
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
-
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+
+    signal treeTotalNodes<== 2**(n+1) - 1;
+    signal treeNodesModel[treeTotalNodes];
+    component hashComps[2**n-1];
+
+    // populate data at deppest  layer which is leaves
+    for (var i = 0; i < 2**n; i++) {
+        treeNodesModel[i] <== leaves[i];
+    }
+
+    for (var j = 0; j < 2**n-1; j++) {
+        hashComps[j] = Poseidon(2);
+        hashComps[j].inputs[0] <== treeNodesModel[2*j];
+        hashComps[j].inputs[1] <== treeNodesModel[2*j+1];
+        treeNodesModel[j+2**n] = hashComps[j].out;
+    }
+
+    root <== hashComps[2**n-2].out;
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -16,4 +33,15 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    component poseidon2[n];
+    signal hash[n+1];
+    hash[0] <== leaf;
+    for (var i=0; i<n; i++) {
+        poseidon2[i] = Poseidon(2);
+        //0: Merkle proof element is in left and hash in the right -> hash(proof+hash), 1: Merkle proof element is in right and hash in the left-> out= hash(hash,proof)
+        poseidon2[i].inputs[0] <== (hash[i] - path_elements[i]) * path_index[i] + path_elements[i];
+        poseidon2[i].inputs[1] <== (path_elements[i] - hash[i]) * path_index[i] + hash[i];
+        hash[i+1] <== poseidon2[i].out;
+    }
+    root <== hash[n];
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -10,20 +10,63 @@ contract MerkleTree is Verifier {
     uint256 public root; // the current Merkle root
 
     constructor() {
-        // [assignment] initialize a Merkle tree of 8 with blank leaves
+        uint leaves=8;
+        for (uint i = 0; i < leaves; i++) {
+            hashes.push();
+        }
+        uint count = leaves;
+        uint offset = 0;
+        while(count > 0) {
+            for(uint i = 0; i < count - 1; i += 2) {
+                hashes.push(PoseidonT3.poseidon([hashes[offset + i], hashes[offset + i + 1]]));
+            }
+            offset += count;
+            count = count / 2;
+        }
+
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
-    }
+        // there are only 8 leaves, starting from 0 index
+        require(index < 8, "Max index is 7");
+        // update the leaf node (level=3)
+        hashes[index] = hashedLeaf;
+        uint256 currentLevelHash = hashedLeaf;
+        uint256 currentIndex=index;
+        uint8 depth=3;
+
+
+        for(uint8 n = 0; n <depth; n ++) {
+            if (currentIndex % 2 == 0) {
+                uint256 hash = PoseidonT3.poseidon([currentLevelHash, hashes[currentIndex+1]]);
+                currentIndex = currentIndex/2 +8;
+                hashes[currentIndex] = hash;
+                currentLevelHash=hash;
+            } else {
+                uint256 hash = PoseidonT3.poseidon([hashes[currentIndex - 1], currentLevelHash]);
+                currentIndex = currentIndex/2 +8;
+                hashes[currentIndex] = hash;
+                currentLevelHash=hash;
+            }
+        }
+
+
+        return currentLevelHash;
+
+        }
+
+
 
     function verify(
-            uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[1] memory input
-        ) public view returns (bool) {
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[1] memory input
+    ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        //inherited from super contract
+        return Verifier.verifyProof(a, b, c, input);
     }
 }
